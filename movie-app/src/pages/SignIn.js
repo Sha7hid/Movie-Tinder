772cb35959d4943ecc44ed82d9a87531b8e70fa2
sha7hid <shahidshafadshahad@gmail.com>
2022-09-20 19:43:08 +0530

    update

--- movie-app/src/pages/SignIn.js
+++ movie-app/src/pages/SignIn.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Layout from '../components/Layout'
 import { supabase } from '../supabase'
 function SignIn() {
+  const [email, setEmail] = useState("");
+ const [message , setMessage] = useState("");
+  const handleSignIn = async (e) => {
+    e.preventDefault()
+
+    const {error} = await supabase.auth.signIn({email})
+
+    if(error){
+      console.log(error)
+    } else{
+ setMessage("Magic Link has been sent.")
+    }
+  }
   return (
     <div>
         <Layout>
+          {message && message}
         <h1>Sign In</h1>
+        <form onSubmit={handleSignIn}>
+          <input type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+          <button type='submit'>Sign In</button>
+        </form>
         </Layout>
      
     </div>
