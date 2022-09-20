c54b95cc2a8b9a626ef2d6500d49bf8a2a464c4b
sha7hid <shahidshafadshahad@gmail.com>
2022-09-20 19:59:19 +0530

    update

--- movie-app/src/supabase.js
+++ movie-app/src/supabase.js
@@ -3,4 +3,4 @@ import {createClient} from '@supabase/supabase-js';
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 
-export const supabase = createClient(supabaseUrl,supabaseUrl); 
+export const supabase = createClient(supabaseUrl,supabaseKey); 
