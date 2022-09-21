bb815d4030443127a175ac6c2f0bdc5a7f8b977f
sha7hid <shahidshafadshahad@gmail.com>
2022-09-21 09:59:22 +0530

    update

---
+++ movie-app/src/auth.js
@@ -0,0 +1,8 @@
+import { supabase } from "./supabase";
+import { useState, useEffect, useContext,createContext} from 'react';
+ 
+const authContext = createContext();
+ 
+export const AuthProvider = ({children}) => {
+    
+}
