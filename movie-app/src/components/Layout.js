dd7dd2b1cb7d017f58b826d1a7c43d4340390f59
sha7hid <shahidshafadshahad@gmail.com>
2022-09-23 19:05:31 +0530

    update

--- movie-app/src/components/Layout.js
+++ movie-app/src/components/Layout.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth} from '../auth'
-
+import '../App.css'
 function Layout({children}) {
   
   const auth = useAuth();
 
   return (
     <div>
-        <header className='header'>
+        <header className="header">
             <ul>
                 <li><Link to={"/"}>Home</Link></li>
                 <li><Link to={"/profile"}>Profile</Link></li>
