d2daafb73e8b35b045c38580e4259858a739fa13
sha7hid <shahidshafadshahad@gmail.com>
2022-09-23 19:14:55 +0530

    UPDATE

--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React ,{useEffect}from 'react'
 import Layout from '../components/Layout'
 
 function Home() {
+  const fetchMovies = async () =>{
+    
+  }
+  useEffect(()=>{
+fetchMovies()
+  },[])
   return (
     <div>
         <Layout>
