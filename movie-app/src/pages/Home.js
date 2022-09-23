541e1d54f5199489d4b031fbde98214c006a7096
sha7hid <shahidshafadshahad@gmail.com>
2022-09-23 19:25:49 +0530

    updt

--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -3,7 +3,12 @@ import Layout from '../components/Layout'
 import axios from 'axios';
 function Home() {
   const fetchMovies = async () =>{
-const {data} = await axios.get("")
+const {data} = await axios.get("https://api.themoviedb.org/3/discover/movie",
+params:{
+  page:Math.random() * 501,
+  api_key:
+}
+)
   }
   useEffect(()=>{
 fetchMovies()
