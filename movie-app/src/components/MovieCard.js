13086479f0a76bb16dceba47b1cb71f68ed568aa
sha7hid <shahidshafadshahad@gmail.com>
2022-09-23 19:58:11 +0530

    updt

---
+++ movie-app/src/components/MovieCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MovieCard({movie}) {
+  return (
+    <div>
+      {movie.title}
+    </div>
+  )
+}
+
+export default MovieCard
--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -1,7 +1,10 @@
-import React ,{useEffect}from 'react'
+import React ,{useEffect, useState}from 'react'
 import Layout from '../components/Layout'
 import axios from 'axios';
+import TinderCard from 'react-tinder-card'
+import MovieCard from '../components/MovieCard';
 function Home() {
+  const [movies, setMovies] = useState([])
   const fetchMovies = async () =>{
 const {data} = await axios.get("https://api.themoviedb.org/3/discover/movie",{
 params:{
@@ -9,16 +12,24 @@ params:{
   api_key:"57e19e5c44a33653ce6bfc54743c9e2e"
   }
 })
-console.log(data);
+
+setMovies(data.results)
   }
   useEffect(()=>{
 fetchMovies()
   },[])
+  const renderMovies = () => {
+return movies.map(movie => {
+  <TinderCard>
+    <MovieCard movie={movie}/>
+  </TinderCard>
+})
+  }
   return (
     <div>
         <Layout>
         <h1>Welcome</h1>
-        
+{renderMovies()}
         </Layout>
      
     </div>
