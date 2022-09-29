e6bbd68579d2b4ac20228f1d433c4f1aacc99d8b
sha7hid <shahidshafadshahad@gmail.com>
2022-09-29 19:30:14 +0530

    updt

--- movie-app/src/pages/Home.js
+++ movie-app/src/pages/Home.js
@@ -5,12 +5,8 @@ import TinderCard from 'react-tinder-card'
 import MovieCard from '../components/MovieCard';
 function Home() {
  const fecthMovies = async () => {
-  const {data} = await axios.get("https://api.themoviedb.org/3/discover/movie",{
-    params:{
-      page:Math.random() * 501,
-      api_key:"57e19e5c44a33653ce6bfc54743c9e2e"
-    }
-  })
+  const {data} = await axios.get("https://api.themoviedb.org/3/discover/movie");
+   
  console.log(data);
 
 }
