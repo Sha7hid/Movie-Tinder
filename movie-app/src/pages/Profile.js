5571a5c828cda117187d0a5554690aa2b153a0cc
sha7hid <shahidshafadshahad@gmail.com>
2022-10-05 19:12:22 +0530

    updt

--- movie-app/src/pages/Profile.js
+++ movie-app/src/pages/Profile.js
@@ -4,6 +4,23 @@ import {supabase} from "../supabase";
 import {useAuth} from "../auth";
 
 function Profile() {
+  const auth = useAuth();
+const getWatchlist = async () => {
+const {data, error} = await supabase 
+.from("watchlists")
+.select()
+.match({user_id: auth.user.id})
+
+if(error){
+  console.log(error)
+}
+
+console.log(data)
+}
+
+  useEffect(() => {
+getWatchlist()
+  })
   return (
  <Layout>
     <h1>Watchlist</h1>
