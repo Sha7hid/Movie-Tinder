2e00c4416a23be7f33b08c738527b648cad92ba1
sha7hid <shahidshafadshahad@gmail.com>
2022-10-05 19:29:19 +0530

    updt

--- movie-app/src/pages/Profile.js
+++ movie-app/src/pages/Profile.js
@@ -2,10 +2,15 @@ import React, {useEffect, useState} from 'react'
 import Layout from "../components/Layout"
 import {supabase} from "../supabase";
 import {useAuth} from "../auth";
+import axios from 'axios';
+import MovieCard from '../components/MovieCard';
 
 function Profile() {
+
   const auth = useAuth();
+const [movies, setMovies] = useState([])
 const getWatchlist = async () => {
+  let movies = [];
 const {data, error} = await supabase 
 .from("watchlists")
 .select("movie_id")
@@ -15,15 +20,32 @@ if(error){
   console.log(error)
 }
 
-console.log(data)
+if(data){
+  for(const movie of data){
+  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${movie.movie_id}`,{
+    params:{
+      api_key:"57e19e5c44a33653ce6bfc54743c9e2e"
+    }
+  })
+movies.push(data)
+}
+setMovies(movies);
+}
 }
 
   useEffect(() => {
 getWatchlist()
   })
+  const renderWatchlist = () => {
+    return movies.map(movie => (
+    <MovieCard movie={movie}/>
+    ))
+  }
   return (
  <Layout>
     <h1>Watchlist</h1>
+
+    {renderWatchlist()}
  </Layout>
   )
 }
